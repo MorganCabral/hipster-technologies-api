f5b12b9eb97816a09ccee8a3c5706cf627e7d337
Morgan Cabral <morgan.cabral@gmail.com>
2015-03-08 18:31:41 -0400

    Added a test module. Moved the application startup logic into its own class to make testing easier.

---
+++ HipsterTechnologies.API/HipsterTechnologies.API.Main.Tests/HipsterApplicationStartupTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Nancy;
+using HipsterTechnologies.API.Main;
+using SimpleLogging.Core;
+using Moq;
+using Nancy.Testing;
+using Nancy.Bootstrapper;
+
+namespace HipsterTechnologies.API.Main.Tests
+{
+    /// <summary>
+    /// Unit Tests for our custom bootstrapper.
+    /// </summary>
+    [TestFixture]
+    public class HipsterApplicationStartupTests
+    {
+        #region Mock Objects
+
+        public Mock<ILoggingService> LoggerMock { get; set; }
+
+        public Browser Browser { get; set; }
+
+        #endregion
+
+        #region Test Module
+
+        class TestModule : NancyModule
+        {
+            public TestModule() : base("tests")
+            {
+                Get["/"] = _ => { return HttpStatusCode.NotImplemented; };
+                Post["/"] = _ => { return HttpStatusCode.NotImplemented; };
+                Delete["/"] = _ => { return HttpStatusCode.NotImplemented; };
+                Put["/"] = _ => { return HttpStatusCode.NotImplemented; };
+            }
+        }
+
+        #endregion
+
+        [SetUp]
+        public void SetUp()
+        {
+            LoggerMock = new Mock<ILoggingService>();
+            var appStartup = new HipsterApplicationStartup(LoggerMock.Object);
+
+            Browser = new Browser(cfg => {
+                cfg.Module<TestModule>();
+                cfg.Dependency<ILoggingService>(LoggerMock.Object);
+                cfg.Dependency<IApplicationStartup>(appStartup);
+            });
+        }
+
+        [Test]
+        public void TestLogsHttpMethodAndEndpoint()
+        {
+            Browser.Get("/tests");
+            Browser.Post("/tests");
+            Browser.Delete("/tests");
+            Browser.Put("/tests");
+
+            LoggerMock.Verify(mock => mock.Info("{0} {1}", "GET", "/tests"), Times.Once);
+            LoggerMock.Verify(mock => mock.Info("{0} {1}", "POST", "/tests"), Times.Once);
+            LoggerMock.Verify(mock => mock.Info("{0} {1}", "DELETE", "/tests"), Times.Once);
+            LoggerMock.Verify(mock => mock.Info("{0} {1}", "PUT", "/tests"), Times.Once);
+
+        }
+    }
+}
---
+++ HipsterTechnologies.API/HipsterTechnologies.API.Main/HipsterApplicationStartup.cs
@@ -0,0 +1,41 @@
+﻿using Nancy.Bootstrapper;
+using SimpleLogging.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HipsterTechnologies.API.Main
+{
+    public class HipsterApplicationStartup : IApplicationStartup
+    {
+        public HipsterApplicationStartup(ILoggingService logger)
+        {
+            _logger = logger;
+        }
+
+        public void Initialize(IPipelines pipelines)
+        {
+            // Trace the logger initialization at the least visible level.
+            // NLog takes forever to load in symbols, so we might as well
+            // do it at application startup to prevent confusion on
+            // the user's part.
+            _logger.Trace("Initialized the logger");
+
+            // Set up the pre-request pipeline.
+            pipelines.BeforeRequest += (ctx) =>
+            {
+                _logger.Info("{0} {1}", ctx.Request.Method.ToUpper(), ctx.Request.Path);
+                _logger.Debug(ctx.Request.Body.ToString());
+                return null;
+            };
+
+            // Set up the post-request pipeline.
+
+            // Set up the error-handler pipeline.
+        }
+
+        private ILoggingService _logger;
+    }
+}
