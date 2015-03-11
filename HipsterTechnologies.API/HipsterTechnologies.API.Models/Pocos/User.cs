cd4f6a35a21a4962d0d44dc9526bc99b83821519
Morgan Cabral <morgan.cabral@gmail.com>
2015-03-11 10:24:12 -0400

    Added fixed user class from the super team adventure force go meeting.

--- HipsterTechnologies.API/HipsterTechnologies.API.Models/Pocos/User.cs
+++ HipsterTechnologies.API/HipsterTechnologies.API.Models/Pocos/User.cs
@@ -4,18 +4,38 @@ using System.Linq;
 using System.Web;
 using Nancy;
 using Nancy.Security;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HipsterTechnologies.API.Models
 {
-    public class User
+
+    /// <summary>
+    /// Model class which represents the user.
+    /// </summary>
+    public class User : IUserIdentity
     {
         /// <summary>
-        /// Primary Key. Facebook User ID.
+        /// Primary Key. Token that facebook generates for our user.
         /// </summary>
         public string UserId { get; set; }
 
+        /// <summary>
+        /// The user's facebook handle.
+        /// </summary>
         public string UserName { get; set; }
 
-        public List<string> Claims { get; set; }
+        /// <summary>
+        /// We don't actually care about claims since we don't make use of them.
+        /// Hence why we have effectively one level of access.
+        /// </summary>
+        [NotMapped]
+        public IEnumerable<string> Claims
+        {
+            get
+            {
+                var claim = new[] { "User" };
+                return new List<String>(claim);
+            }
+        }
     }
 }
