657edfa6d1e626f6510a67546f466818ed208624
Morgan Cabral <morgan.cabral@gmail.com>
2015-03-11 10:21:04 -0400

    Created an interface for the IModuleContext class. I don't believe anything is using this just yet but it will clean up code a bit when we do get around to that.

---
+++ HipsterTechnologies.API/HipsterTechnologies.API.Routes/Context/IModuleContext.cs
@@ -0,0 +1,31 @@
+﻿using HipsterTechnologies.API.Models.Contexts;
+using SimpleLogging.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HipsterTechnologies.API.Routes.Context
+{
+    /// <summary>
+    /// Module-level context object.
+    /// </summary>
+    public interface IModuleContext
+    {
+        /// <summary>
+        /// Access to a logger object.
+        /// </summary>
+        ILoggingService Logger { get; set; }
+
+        /// <summary>
+        /// Access to the ModelContext factory object.
+        /// </summary>
+        IModelContextFactory DbFactory { get; set; }
+
+        /// <summary>
+        /// Get the API Version. Use this for versioning in routes.
+        /// </summary>
+        String ApiVersion { get; }
+    }
+}
